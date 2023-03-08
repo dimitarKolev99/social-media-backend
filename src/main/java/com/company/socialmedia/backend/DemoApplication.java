ef5af25701d0836442879ed002f5451aeb9f2476
Dimitar <mitko9966@gmail.com>
2023-03-08 17:15:12 +0100

    prepare for deployment

--- src/main/java/com/company/socialmedia/backend/DemoApplication.java
+++ src/main/java/com/company/socialmedia/backend/DemoApplication.java
@@ -24,21 +24,6 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@OpenAPIDefinition(
-			info = @Info(
-					title = "Code-First Approach (reflectoring.io)",
-					description = "" +
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et rhoncus quam. Aenean quis augue ac eros pulvinar malesuada. " +
-							"In sagittis elit egestas tincidunt iaculis. " +
-							"Donec eu lacus vitae nulla varius consectetur a vel quam. Aliquam erat volutpat. Duis eget ullamcorper tellus",
-					contact = @Contact(name = "Reflectoring", url = "https://reflectoring.io", email = "petros.stergioulas94@gmail.com"),
-					license = @License(name = "MIT Licence", url = "https://github.com/thombergs/code-examples/blob/master/LICENSE")),
-			servers = @Server(url = "http://localhost:8080")
-	)
-	@SecurityScheme(name = "api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
-	class OpenAPIConfiguration {
-	}
-
 	@Override
 	public void run(String... arg) throws Exception {
 //    storageService.deleteAll();
